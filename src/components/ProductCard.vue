ba2358731306934d5443148678a0305de5fd62f2
eugene <eugenecodedev@gmail.com>
2022-01-29 22:14:46 +0300

    init project stoped at 45:54

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,35 @@
+<template >
+  <div class="column is-3" >
+        <div class="box" >
+          <figure class="image mb-4" >
+            <img :src="product.image" :alt="product.name" >
+          </figure >
+          <h3 class="is-size-4" >
+            {{ product.name }}
+          </h3 >
+          <p class="is-size-6 has-text-grey" >KES {{ product.price }}</p >
+          View Details
+        </div >
+      </div >
+</template >
+
+<script >
+export default {
+  name: "ProductCard",
+  props:{
+    product:{
+      required: true,
+      type: Object,
+    },
+
+  }
+}
+</script >
+
+<style scoped >
+.image {
+  margin-top: -1.125rem;
+  margin-left: -1.125rem;
+  margin-right: -1.125rem;
+}
+</style >
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,53 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
+<template >
+  <div class="home" >
+    <section class="hero is-medium is-dark mb-6" >
+      <div class="hero-body has-text-centered" >
+        <p class="title mb-6" >Welcome to Djacket</p >
+        <div class="subtitle" >
+          The baddest jacket store in kenya online
+        </div >
+      </div >
+    </section >
+    <div class="column is-multiline" >
+      <div class="column is-12" >
+        <h2 class="is-size-2 has-text-centered" >
+          Latest products
+        </h2 >
+      </div >
+      <div class="row" >
+        <ProductCard v-for="product in latestProducts" v-bind:key="product.id" :product="product" ></ProductCard >
+      </div >
+    </div >
+  </div >
+</template >
 
-<script>
+<script >
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
+// import ProductCard from '../components/ProductCard'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      latestProducts: []
+    }
+  },
+  // components: {
+  //   ProductCard
+  // },
+  mounted() {
+    this.getLatestProperties();
+  },
+  methods: {
+    getLatestProperties() {
+      axios.get('/api/v1/latest_products/').then((response) => {
+        console.log(response.data);
+        this.latestProducts = response.data;
+      }).catch((response) => {
+        console.log(response.err)
+      })
+    }
   }
 }
-</script>
+</script >
